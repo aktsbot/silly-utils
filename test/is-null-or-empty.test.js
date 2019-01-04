01e0e1febc447fa7d82fd3f5444fa01e8eed8e29
akts <box.ashishk@gmail.com>
2019-01-05 00:40:16 +0530

    more test coverage

--- test/is-null-or-empty.test.js
+++ test/is-null-or-empty.test.js
@@ -1,8 +1,20 @@
 const test = require('ava')
 const isNullOrEmpty = require('../src/is-null-or-empty')
 
-test('checks if a variable is null or empty', t => {
+test('checks if a variable is empty string', t => {
   const item = ''
+  const status = isNullOrEmpty(item)
+  t.deepEqual(status, true)
+})
+
+test('checks if a variable is null', t => {
+  const item = null
+  const status = isNullOrEmpty(item)
+  t.deepEqual(status, true)
+})
+
+test('checks if a variable is undefined', t => {
+  const item = undefined
   const status = isNullOrEmpty(item)
   t.deepEqual(status, true)
 })
